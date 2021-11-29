be9918eb0cdaaf336d9dc35cd2ce84295dfa2ade
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-29 14:50:03 -0600

    Java

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = "Ian";
+        String searchQuery = request.getParameter("q");
+
+
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+        out.printf("<h1>Hello, %s!</h1>", name);
+
+
+        if(searchQuery != null){
+            out.println("<p>Whats up?" + searchQuery + "<p>");
+        }
     }
+// ?q=Ian
 }
