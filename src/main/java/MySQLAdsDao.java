66e83ba3c0e28fe4c09b8317b3c074975ec706d8
Ian McConihay <mcconihay.ian92@gmail.com>
2021-12-07 11:19:40 -0600

    jdbc finish

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -54,23 +54,13 @@ private Connection connection;
 	@Override
 	public Long insert(Ad ad) {
 		try {
-			String query = "INSERT INTO ads(id, title, description) VALUES (?, ?, ?)";
-			PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-
-			statement.setLong(1, ad.getUserId());
-			statement.setString(2, ad.getTitle());
-			statement.setString(3, ad.getDescription());
-
-			statement.executeUpdate();
-
-			ResultSet resultSet = statement.getGeneratedKeys();
-			resultSet.next();
-
-			return resultSet.getLong(1);
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+			ResultSet rs = stmt.getGeneratedKeys();
+			rs.next();
+			return rs.getLong(1);
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new RuntimeException("Error creating a new ad.", e);
 		}
-
-		return null;
 	}
 }
